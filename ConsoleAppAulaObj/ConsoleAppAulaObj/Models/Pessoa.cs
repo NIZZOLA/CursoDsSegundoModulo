09a2e20f4ffcd59eb19c9e47593f5d3bad0913d4
Marcio R Nizzola <marcio@nizzola.com.br>
2020-08-27 20:43:52 -0300

    Aula C# conceitos sobre string e datetime + objetos

---
+++ ConsoleAppAulaObj/ConsoleAppAulaObj/Models/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppAulaObj.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime Nascimento { get; set; }
+
+        public char Sexo { get; set; }
+
+    }
+}
