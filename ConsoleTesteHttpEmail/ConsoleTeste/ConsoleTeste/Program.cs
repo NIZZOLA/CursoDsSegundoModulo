e23d94ac2603cc23bdd577e8a3c9442a3f83d671
Marcio R Nizzola <marcio@nizzola.com.br>
2020-11-13 06:49:33 -0300

    aula dia 12/11/2020

---
+++ ConsoleTesteHttpEmail/ConsoleTeste/ConsoleTeste/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleTeste
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string texto = "O Palmeiras não tem mundial!";
+
+            
+
+            Console.WriteLine(Funcoes.FormataMaiusculo(texto));
+
+            Console.WriteLine("Pós:" + texto);
+
+
+            Console.WriteLine(Funcoes.FormataMinusculo(texto));
+
+        //    var resposta = await Funcoes.ConsultaCep("13300170");
+
+        //    Console.WriteLine(resposta);
+
+
+            var resposta2 = Funcoes.SendMail("marcionizzola@gmail.com", "Prof Nizzola", "marcio.nizzola@etec.sp.gov.br", "Nizzola Prof", "marcionizzola@gmail.com",
+                  "", "smtp.gmail.com", true, "E-MAIL TESTE DA AULA", "O Palmeiras não tem mundial!");
+
+            if (resposta2)
+                Console.WriteLine("Sucesso no envio!");
+            else
+                Console.WriteLine("Erro no envio!");
+        }
+
+        
+
+    }
+
+    
+}
